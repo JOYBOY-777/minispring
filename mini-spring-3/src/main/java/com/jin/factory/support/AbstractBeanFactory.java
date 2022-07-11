86550a8128cbb47f5d3c76d80b4c53949c5e3f91
JOYBOY <1781073841@qq.com>
2022-07-11 15:14:42 +0800

    修改万恶之源

--- mini-spring-3/src/main/java/com/jin/factory/support/AbstractBeanFactory.java
+++ mini-spring-3/src/main/java/com/jin/factory/support/AbstractBeanFactory.java
@@ -4,6 +4,8 @@ import com.jin.BeansException;
 import com.jin.factory.BeanFactory;
 import com.jin.factory.config.BeanDefinition;
 
+import java.util.Base64;
+
 /*
 * 关键的抽象工厂类，里面定义几种模板方法，为了以后给他的子类自定义实现,抽象类不必实现接口，也可以实现接口中全部方法
 * 或者部分方法，如果实现的是部分的方法，如果一个类只需要用到接口中的部分方法，那么让他去继承这个类就可以用接口中的部分方法了，
@@ -11,22 +13,29 @@ import com.jin.factory.config.BeanDefinition;
 * */
 public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
     @Override
+    //通过无参构造获取bean
     public Object getBean(String name) throws BeansException {
-        //对单例bean对象的获取
-        Object bean = getSingleton(name);
-        if(bean!=null) return bean;
-        //拿不到的话就去那bean的定义，根据名字还有bean定义去实例化Bean
-        BeanDefinition beanDefinition = getBeanDefinition(name);
-        return createBean(name,beanDefinition);
-
+        return doGetBean(name,null);
     }
 
     @Override
+    //通过有参构造获取bean
     public Object getBean(String name, Object... ages) throws BeansException {
-
-        return null;
+        return doGetBean(name,ages);
     }
 
+    //提供有参或者无参的方式获取bean
+    protected <T> T doGetBean(final String name,final Object[] args){
+        //首先尝试去获取单例的bean
+        Object bean = getSingleton(name);
+        if (bean != null) return (T) bean;
+        //如果拿不到的话就去拿Bean的定义，然后在去创建一个bean
+        BeanDefinition beanDefinition = getBeanDefinition(name);
+        return (T) createBean(name,beanDefinition,args);
+    }
+
+    //根据bean名字去获取bean的定义
     protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
-    protected abstract Object createBean(String beanName,BeanDefinition beanDefinition) throws BeansException;
+    //根据bean的名字，bean包装的定义还有构造器的参数等来创建一个bean，这个参数可以传空
+    protected abstract Object createBean(String beanName,BeanDefinition beanDefinition,Object[] args) throws BeansException;
 }
--- mini-spring-3/src/main/java/com/jin/factory/support/InstantiationStrategy.java
+++ mini-spring-3/src/main/java/com/jin/factory/support/InstantiationStrategy.java
@@ -7,5 +7,6 @@ import java.lang.reflect.Constructor;
 
 //这个接口提供实例化策略接口，一种是在JDK中自带的动态代理，一种是用CGLIB进行动态代理
 public interface InstantiationStrategy {
+    //既然是bean的实例化策略那么参数就是bean的定义，bean名称，构造器，以及构造起的参数等
     Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor,Object[] args) throws BeansException;
 }
